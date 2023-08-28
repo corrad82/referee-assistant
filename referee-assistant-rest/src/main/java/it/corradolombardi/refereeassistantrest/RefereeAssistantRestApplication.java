7c0bddf23a8d30630435b49fdc5978f11b9e47fe
corrad82 <corrado.lombardi@gmail.com>
2023-08-28 20:31:59 +0200

    Initial commit

---
+++ referee-assistant-rest/src/main/java/it/corradolombardi/refereeassistantrest/RefereeAssistantRestApplication.java
@@ -0,0 +1,13 @@
+package it.corradolombardi.refereeassistantrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefereeAssistantRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefereeAssistantRestApplication.class, args);
+	}
+
+}
---
+++ referee-assistant-rest/src/test/java/it/corradolombardi/refereeassistantrest/RefereeAssistantRestApplicationTests.java
@@ -0,0 +1,15 @@
+package it.corradolombardi.refereeassistantrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefereeAssistantRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+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
