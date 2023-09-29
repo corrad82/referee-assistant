6a265be433de2f7fbec42098e27b59d8dd577cf2
corrad82 <corrado.lombardi@gmail.com>
2023-09-30 00:17:42 +0200

    wip

--- referee-assistant-core/src/main/java/it/corradolombardi/refereeassistant/core/Season.java
+++ referee-assistant-core/src/main/java/it/corradolombardi/refereeassistant/core/Season.java
@@ -1,14 +1,16 @@
 package it.corradolombardi.refereeassistant.core;
 
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
+import java.time.LocalDate;
+
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class League {
-    private final String code;
-    private final Gender gender;
-    private final Season season;
-    private final boolean restricted;
+public class Season {
+    private final String id;
+    private final LocalDate starDate;
+    private final LocalDate endDate;
 }
--- referee-assistant-rest/src/main/java/it/corradolombardi/refereeassistant/rs/v1/SeasonsController.java
+++ referee-assistant-rest/src/main/java/it/corradolombardi/refereeassistant/rs/v1/SeasonsController.java
@@ -1,6 +1,6 @@
 package it.corradolombardi.refereeassistant.rs.v1;
 
-import it.corradolombardi.refereeassistant.core.Match;
+import it.corradolombardi.refereeassistant.core.Season;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/matches")
-public class MatchesController {
+@RequestMapping("/seasons")
+public class SeasonsController {
 
 //    https://www.baeldung.com/spring-hateoas-tutorial
+//    https://www.baeldung.com/spring-hateoas-tutorial
+    @GetMapping
+    public ResponseEntity<CollectionModel<Season>> findAll() {
 
-//    @GetMapping
-//    public ResponseEntity<CollectionModel<Match>> findAll() {
-//
-//        CollectionModel<Match> matches = CollectionModel.empty();
-//        return ResponseEntity.ok(matches);
-//    }
+    }
 }
